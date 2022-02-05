4dec36055e9e075ef0ade6307202974c805f597f
IbrahimTss <99019500+IbrahimTss@users.noreply.github.com>
2022-02-06 00:23:36 +0530

    used params

--- login.js
+++ login.js
@@ -1,15 +1,32 @@
-const signUp = e => {
+// const signUp = e => {
     
-    const form ={
-        name:document.getElementById('name').value,
-        email:document.getElementById('email').value,
-        password:document.getElementById('password').value
+//     const form ={
+//         name:document.getElementById('name').value,
+//         email:document.getElementById('email').value,
+//         password:document.getElementById('password').value
 
-    }
+//     }
 
-    localStorage.setItem('form',JSON.stringify(form));
-    document.write = localStorage.getItem('form');
+//     localStorage.setItem('form',JSON.stringify(form));
+//     // document.write = localStorage.getItem('form');
+//     frmData();
+//     e.preventDefault();
 
-}
+// }
+
+
+
+// const logIn = e =>{
+//     const name = document.getElementById('name').value;
+//     const email = document.getElementById('email').value;
+//     const password = document.getElementById('password').value;
+
+//     localStorage.setItem('name',name);
+//     localStorage.setItem('emial',email);
+//     localStorage.setItem('password',password);
+
+//     return;
+// }
+
 
 
---
+++ logindata.js
@@ -0,0 +1,15 @@
+
+
+window.addEventListener('load',()=>{
+
+    const params = (new URL(document.location)).searchParams;
+    const name = params.get('name');
+    const email = params.get('email');
+    const password = params.get('password');
+
+    document.getElementById('name').innerHTML = name ;
+    document.getElementById('email').innerHTML = email ;
+    document.getElementById('password').innerHTML = password ;
+
+    
+})
